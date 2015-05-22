02b268b12311b332ef9f943774fd7176499b64d9
wang zuo <wzuoadjusted@gmail.com>
2015-05-22 13:10:49 +0800

    a better example

--- example/app.js
+++ example/app.js
@@ -3,12 +3,25 @@ require('medium-editor/dist/css/themes/default.css');
 var React = require('react');
 var Editor = require('../lib/editor');
 
-var text = 'Fusce dapibus, tellus ac cursus commodo';
-var handleChange = function(text) {
-  console.log(text);
-};
+var App = React.createClass({
+  getInitialState() {
+    return {text: 'Fusce dapibus, tellus ac cursus commodo'}
+  },
 
-React.render(
-  <Editor text={text}
-    onChange={handleChange}
-    options={{buttons: ['bold', 'italic', 'underline']}}/>, document.body);
+  render() {
+    return (
+      <div>
+        <div>{this.state.text}</div>
+        <Editor text={this.state.text}
+          onChange={this.handleChange}
+          options={{buttons: ['bold', 'italic', 'underline']}}/>
+      </div>
+    );
+  },
+
+  handleChange(text) {
+    this.setState({text: text});
+  }
+});
+
+React.render(<App/>, document.body);
