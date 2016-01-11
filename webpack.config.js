bf8605e10126bbade2bc1840374e24aecd43b1b5
Wang Zuo <wzuoadjusted@gmail.com>
2016-01-11 13:06:02 +0800

    add builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,12 @@ var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 
 module.exports = {
   entry: {
-    'dist/editor': './lib/index',
     'example/bundle': './example/index.js',
     'example/styles-bundle': './example/styles.js'
   },
   output: {
-    path: __dirname,
     filename: '[name].js',
+    path: __dirname + '/builds',
     libraryTarget: 'umd'
   },
   module: {
